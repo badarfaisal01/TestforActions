798530d9d059a404e4a3a6523eb7869ac28a76be
badarfaisal01 <badarfaisal98@gmail.com>
2025-05-18 20:58:33 +0500

    test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Hello, World!");
 console.log("This is a simple Node.js application.");
 
-
+console.log("This application demonstrates how to use Node.js for server-side JavaScript.");
